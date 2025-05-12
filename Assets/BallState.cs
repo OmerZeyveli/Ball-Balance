2e2e00c1498df4c1725af07082697c04d9b75070
meymann <mustafaesgunoglu8@gmail.com>
2025-05-12 18:33:40 +0300

    Update BallState.cs

--- Assets/BallState.cs
+++ Assets/BallState.cs
@@ -2,12 +2,12 @@ using UnityEngine;
 
 public class BallState : MonoBehaviour
 {
-    public bool dropped = false;
+    public bool dropped = false; // Has the ball dropped?
 
     void OnCollisionEnter(Collision other) {
-        if(other.gameObject.CompareTag("drop"))
+        if(other.gameObject.CompareTag("drop")) // Hit "drop" object?
         {
-            dropped = true;
+            dropped = true; // Mark as dropped
         }
     }
 }
